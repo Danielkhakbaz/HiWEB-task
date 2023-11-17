e5ddce84a0410d0f76d2c387a15420c1f25d1864
Danial <danial@Danials-MacBook-Pro.local>
2023-11-17 22:40:25 +0330

    FIX: Fix the warning about using old tailwindCSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from "tailwindcss";
 
-const colors = require("tailwindcss/colors");
-
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,23 +7,24 @@ const config: Config = {
     "./layout/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      ...colors,
-      hiwebGreen: {
-        500: "#46B666",
-        700: "#3A9E57",
-        900: "#328C4C",
-      },
-      hiwebRed: {
-        500: "#FF6666",
-      },
-      hiwebGray: {
-        100: "#CCCCCC",
-        200: "#ABABAB",
-        300: "#A0A0A0",
-        400: "#9A9A9A",
-        500: "#5C5C5C",
-        700: "#666666",
+    extend: {
+      colors: {
+        hiwebGreen: {
+          500: "#46B666",
+          700: "#3A9E57",
+          900: "#328C4C",
+        },
+        hiwebRed: {
+          500: "#FF6666",
+        },
+        hiwebGray: {
+          100: "#CCCCCC",
+          200: "#ABABAB",
+          300: "#A0A0A0",
+          400: "#9A9A9A",
+          500: "#5C5C5C",
+          700: "#666666",
+        },
       },
     },
   },
